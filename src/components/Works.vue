2d491d550febef7bf3c03fc48db5e526d87a6f21
JoeTakasuna <jonpili99@gmail.com>
2019-09-10 13:01:40 +0900

    技術と作品ページ作成

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,15 +1,20 @@
 <template lang="pug">
   .container
     h1 技術と作品
+    WorkCard(width="240px" height="240px")
 </template>
 
 <script>
+import WorkCard from './parts/WorkCard.vue'
+
 export default {
-  name: 'Works'
+  name: 'Works',
+  components: {
+    WorkCard
+  }
 };
 </script>
 
 <style scoped>
-.container {
-}
+
 </style>
---
+++ src/components/parts/WorkCard.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  .container
+    v-card.card(
+      v-for="(content, index) in contents"
+      :content="content"
+      :key="index"
+      :width="width"
+      :height="height"
+      )
+      v-img(:src="content.img")
+      v-card-title {{ content.title }}
+      v-card-text {{ content.text }}
+</template>
+
+<script>
+export default {
+  name: 'WorkCard',
+  data () {
+    return {
+      contents: [
+        {
+          title: '酒場',
+          text: 'おさけがのめます',
+          img: require('@/assets/works/1.jpg'),
+        },
+        {
+          title: '遺跡',
+          text: 'ひみつがあります',
+          img: require('@/assets/works/2.jpg'),
+        },
+        {
+          title: '火山',
+          text: 'だいちをつくります',
+          img: require('@/assets/works/3.jpg'),
+        }
+      ]
+    }
+  },
+  props: {
+    width: String,
+    height: String
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.card {
+  margin: 12px;
+}
+</style>
