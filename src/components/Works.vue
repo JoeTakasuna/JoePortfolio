f1d7c83565cd646fe57b103a1e27432f91367c2d
JoeTakasuna <jonpili99@gmail.com>
2019-09-10 13:05:33 +0900

    検索ボックス追加

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   .container
     h1 技術と作品
+    .search
+      .search-box
+        v-select(label="技術で検索" :items="techs" multiple)
+      .search-box
+        v-text-field(label="キーワード検索" prepend-inner-icon="search")
     WorkCard(width="240px" height="240px")
 </template>
 
@@ -11,10 +16,21 @@ export default {
   name: 'Works',
   components: {
     WorkCard
+  },
+  data () {
+    return {
+      techs: ['Vue.js', 'Python', 'GAS']
+    }
   }
 };
 </script>
 
 <style scoped>
-
+.search {
+  display: flex;
+}
+.search-box {
+  max-width: 30vw;
+  margin: 12px;
+}
 </style>
