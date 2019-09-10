6d58b174b23c93e02bebe775d6efc40a9c8835d5
JoeTakasuna <jonpili99@gmail.com>
2019-09-10 16:16:16 +0900

    4種のデータ取得

--- src/components/parts/PostDataList.vue
+++ src/components/parts/PostDataList.vue
@@ -3,13 +3,22 @@
     v-container
       v-layout(wrap)
         v-flex(xs12 sm6 md3)
-          v-card github
+          v-card.card
+            ul
+              li(v-for="data in githubData") {{ data.name }}
         v-flex(xs12 sm6 md3)
-          v-card {{ message }} {{ items }}
+          v-card.card
+            a(class="qiita" href="https://qiita.com/jonpili" data-qiita-widget data-username="jonpili" data-hide-footer) Qiita投稿
         v-flex(xs12 sm6 md3)
-          v-card twitter
+          v-card.card
+            a(class="twitter-timeline" data-height="400" href="https://twitter.com/jonpili?ref_src=twsrc%5Etfw") Tweets by jonpili
         v-flex(xs12 sm6 md3)
-          v-card real twitter
+          v-card.card
+            a(class="twitter-timeline" data-height="400" href="https://twitter.com/RealTweet_Joe?ref_src=twsrc%5Etfw") Tweets by RealTweet_Joe
+        v-flex(xs12 sm12 md12)
+          v-card.card
+            a.frame(href="https://github.com/JoeTakasuna" target="_blank")
+              img.img(src="https://grass-graph.moshimo.works/images/JoeTakasuna.png")
 </template>
 
 <script>
@@ -20,29 +29,60 @@ export default {
   data () {
     return {
       items: null,
-      message: ''
+      message: '',
+      githubData: null
     }
   },
   created: function() {
-    this.message = 'Loading...'
-    var vm = this
-    var params = { page: 1, per_page: 20, query: 'Vue.js' }
-    axios.get('https://qiita.com/api/v2/items', { params })
+    axios.get('https://api.github.com/users/JoeTakasuna/repos')
       .then(response => {
-        console.log(response.data)
-        vm.items = response.data
+        this.githubData = response.data
       })
       .catch(error => {
-        vm.message = 'Error!' + error
+        this.message = 'Error!' + error
       })
       .finally(() => { // これ可能？
-        vm.message = ''
+        this.message = ''
       })
+  },
+  mounted: function() {
+    if (!document.getElementById('qiita-wjs')) {
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0]
+        if (d.getElementById(id)) return
+        js = d.createElement(s)
+        js.id = id
+        js.src = 'https://qiita-widget.suin.io/widget.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      })(document, 'script', 'qiita-wjs')
+    }
+
+    if (!document.getElementById('twitter-wjs')) {
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0]
+        if (d.getElementById(id)) return
+        js = d.createElement(s)
+        js.id = id
+        js.src = 'https://platform.twitter.com/widgets.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      })(document, 'script', 'twitter-wjs')
+    }
   }
-};
+}
 </script>
 
 <style scoped>
-.container {
+.card {
+  margin: 6px;
+}
+.frame {
+  display: flex;
+  justify-content: center;
+}
+.img {
+  margin: 6px;
+  max-width: 100%;
 }
 </style>
