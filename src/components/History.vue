cc0086ca8455322c9bc2c1ab312457b4bf674aa9
JoeTakasuna <jonpili99@gmail.com>
2019-09-10 11:34:00 +0900

    経歴の作成

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,16 +1,63 @@
 <template lang="pug">
   .container
     h1 経歴
+    .search
+      .search-box
+        v-select(label="ジャンル検索" :items="genre" multiple)
+      .search-box
+        v-text-field(label="キーワード検索" prepend-inner-icon="search")
+    v-timeline()
+      TimelineCard(v-for="(content, index) in contents" :content="content" :key="index")
 </template>
 
 <script>
+import TimelineCard from './parts/TimelineCard.vue'
+
 export default {
-  name: 'History'
+  name: 'History',
+  components: {
+    TimelineCard
+  },
+  data () {
+    return {
+      contents: [
+        {
+          title: 'サイト作成',
+          text: 'わーーーーい',
+          date: '2019',
+          color: 'red'
+        },
+        {
+          title: '大学入学',
+          text: 'やったーーーーーーー',
+          date: '2013',
+          color: 'blue'
+        },
+        {
+          title: '誕生',
+          text: 'いえーーーーーーい',
+          date: '1994',
+          color: 'green'
+        }
+      ],
+      genre: ['IT', '建築', '趣味']
+    }
+  }
 };
 </script>
 
 <style scoped>
 .container {
-  margin: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.search {
+  display: flex;
+  flex-direction: row;
+}
+.search-box {
+  max-width: 30vw;
+  margin: 12px;
 }
 </style>
---
+++ src/components/parts/TimelineCard.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  v-timeline-item(small :color="content.color")
+    template(v-slot:opposite)
+      span(:class="`${content.color}--text`") {{ content.date }}
+    v-card
+      v-card-title(:class="`${content.color}--text`") {{ content.title }}
+      v-card-text {{ content.text }}
+</template>
+
+<script>
+export default {
+  name: 'TimelineCard',
+  props: {
+    content: Object
+  }
+};
+</script>
+
+<style scoped>
+.container {
+}
+</style>
