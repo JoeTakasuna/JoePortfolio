1e3bd68426e9065e094a974ecb0f7f2425230546
JoeTakasuna <jonpili99@gmail.com>
2019-09-10 19:24:05 +0900

    データの置き場所を移動

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -6,7 +6,8 @@
         v-select(label="技術で検索" :items="techs" multiple)
       .search-box
         v-text-field(label="キーワード検索" prepend-inner-icon="search" v-model="keyword")
-    WorkCard(width="240px" height="240px" :keyword="keyword")
+    .card-area(v-for="content in filteredContents")
+      WorkCard(:content="content" width="240px" height="240px")
 </template>
 
 <script>
@@ -19,8 +20,51 @@ export default {
   },
   data () {
     return {
+      contents: [
+        {
+          title: '酒場',
+          text: 'おさけがのめます',
+          img: require('@/assets/works/1.jpg'),
+        },
+        {
+          title: '遺跡',
+          text: 'ひみつがあります',
+          img: require('@/assets/works/2.jpg'),
+        },
+        {
+          title: '火山',
+          text: 'だいちをつくります',
+          img: require('@/assets/works/3.jpg'),
+        }
+      ],
+      filteredContents: null,
       techs: ['Vue.js', 'Python', 'GAS'],
-      keyword: ''
+      keyword: '',
+      message: ''
+    }
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.keyword === '') {
+        this.message = ''
+        this.filteredContents = this.contents
+        return
+      }
+      this.message = ''
+      this.filteredContents = [this.contents[1], this.contents[2]]
+    },
+    shuffle: function () {
+      this.filteredContents = _.shuffle(this.filteredContents)
+    }
+  },
+  created: function() {
+    this.filteredContents = this.contents
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+  },
+  watch: {
+    keyword: function() {
+      this.message = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
     }
   }
 };
@@ -33,5 +77,10 @@ export default {
 .search-box {
   max-width: 30vw;
   margin: 12px;
+}
+.card-area {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
--- src/components/parts/WorkCard.vue
+++ src/components/parts/WorkCard.vue
@@ -1,18 +1,8 @@
 <template lang="pug">
-  .container
-    v-btn(@click="shuffle") Shuffle
-    transition-group(name="flip-list")
-      v-card.card(
-        v-for="(content, index) in filteredContents"
-        :content="content"
-        :key="content"
-        :width="width"
-        :height="height"
-        )
-        v-img(:src="content.img")
-        v-card-title {{ content.title }}
-        v-card-text {{ content.text }}
-    p {{ message }}
+  v-card.card(:content="content" :width="width" :height="height")
+    v-img(:src="content.img")
+    v-card-title {{ content.title }}
+    v-card-text {{ content.text }}
 </template>
 
 <script>
@@ -20,71 +10,19 @@ import _ from 'lodash'
 
 export default {
   name: 'WorkCard',
-  data () {
-    return {
-      message: '',
-      contents: [
-        {
-          title: '酒場',
-          text: 'おさけがのめます',
-          img: require('@/assets/works/1.jpg'),
-        },
-        {
-          title: '遺跡',
-          text: 'ひみつがあります',
-          img: require('@/assets/works/2.jpg'),
-        },
-        {
-          title: '火山',
-          text: 'だいちをつくります',
-          img: require('@/assets/works/3.jpg'),
-        }
-      ],
-      filteredContents: null
-    }
-  },
   props: {
+    content: Object,
     width: String,
-    height: String,
-    keyword: String
-  },
-  methods: {
-    getAnswer: function() {
-      if (this.keyword === '') {
-        this.message = ''
-        this.filteredContents = this.contents
-        return
-      }
-      this.message = ''
-      this.filteredContents = [this.contents[1], this.contents[2]]
-    },
-    shuffle: function () {
-      this.filteredContents = _.shuffle(this.filteredContents)
-    }
-  },
-  created: function() {
-    this.filteredContents = this.contents
-    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
-  },
-  watch: {
-    keyword: function() {
-      this.message = 'Waiting for you to stop typing...'
-      this.debouncedGetAnswer()
-    }
+    height: String
   }
 };
 </script>
 
 <style scoped>
-.container {
-  /* display: flex;
-  flex-direction: row;
-  flex-wrap: wrap; */
-}
 .card {
   margin: 12px;
 }
-.flip-list-move {
+/* .flip-list-move {
   transition: transform 1s;
-}
+} */
 </style>
