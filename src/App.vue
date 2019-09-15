b5936f7ebe707678d029aeb8db88ad2b0b6b51f2
JoeTakasuna <jonpili99@gmail.com>
2019-09-15 11:25:46 +0900

    基本カラーの設定とisMobileによるメニューバー切り替え

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,88 @@
 <template>
   <v-app id="app">
     <v-content>
-      <HeaderMenu />
+      <HeaderMenuOnMobile v-if="isMobile" />
+      <HeaderMenu v-else />
       <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
+import HeaderMenuOnMobile from './components/parts/HeaderMenuOnMobile.vue'
 import HeaderMenu from './components/parts/HeaderMenu.vue'
 
 export default {
   name: 'App',
   components: {
+    HeaderMenuOnMobile,
     HeaderMenu
-  }
-  // data () {
-  //   return {
-  //     xxx: データの置き場所
-  //   }
-  // },
+  },
+  // data () => ({
+  //   xxx: データの置き場所
+  // }),
   // props: {
   //   xxx: データの受け取り
   // },
   // computed: {
-  //   xxx: function() {
+  //   xxx: () {
   //     return データの自動算出
   //   }
   // },
   // watch: {
-  //   xxx: function() {
+  //   xxx: () {
   //     データ変更時にメソッドを実行
   //   }
   // },
   // methods: {
-  //   xxx: function() {
+  //   xxx: () {
   //     メソッドの組み込み
   //   }
   // },
-  // created: function() {
+  // created: () {
   //   インスタンスが作成された後に同期的に呼ばれるフック
   // },
-  // mounted: function() {
+  // mounted: () {
   //   インスタンスがマウントされたちょうど後に呼ばれるフック
-  // }
+  // },
+  data: () => ({
+    isMobile: false,
+  }),
+  beforeDestroy () {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize, { passive: true })
+    }
+  },
+  mounted () {
+    this.onResize()
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+  methods: {
+    onResize () {
+      this.isMobile = window.innerWidth < 600
+    },
+  }
 };
 </script>
 
 <style>
 :root {
-  --main: #86B81B;
-  --dark: #2E3819;
-  --light: #A1C358;
-  --accent: #8F1CB8;
+  /* light-green */
+  --main: #8BC34A;
+  /* light-green darken-4 */
+  --dark: #33691E;
+  /* light-green lighten-4 */
+  --light: #DCEDC8;
+  /* deep-purple */
+  --accent: #673AB7;
+  /* grey darken-4 */
   --text: #000A02;
-  --back: #FCFAF2;
+  /* grey lighten-4 */
+  --back: #F5F5F5;
 }
 #app {
   color: var(--text);
   background-color: var(--back);
-  padding: 0 10vw;
+  padding: 0 5vw;
 }
 </style>
