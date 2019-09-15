c71c5ff3a43b712f67e9810e18a1799731fc067a
JoeTakasuna <jonpili99@gmail.com>
2019-09-15 12:58:38 +0900

    メニューの完成

--- src/components/parts/HeaderMenuOnMobile.vue
+++ src/components/parts/HeaderMenuOnMobile.vue
@@ -2,37 +2,59 @@
   .header-menu
     v-navigation-drawer(v-model="drawer" app right)
       v-list
-        v-list-item
+        v-list-item(v-for="linkItem in linkItems"
+                    :key="linkItem.name"
+                    @click="toLink(linkItem.path)")
+          v-list-item-action
+            v-icon {{ linkItem.icon }}
           v-list-item-content
-            router-link.link(to="/") home
-        v-list-item
-          v-list-item-content
-            router-link.link(to="/history") history
-      router-link.link(to="/works")
-        v-btn.btn(small outlined) works
-      router-link.link(to="/analytics")
-        v-btn.btn(small outlined) analytics
-      router-link.link(to="/future")
-        v-btn.btn(small outlined) future
+            v-list-item-title {{ linkItem.name }}
     v-app-bar.app-bar(app color="light-green")
       div(class="flex-grow-1")
-      v-app-bar-nav-icon(@click.stop="drawer = !drawer")
+      v-app-bar-nav-icon(@click.stop="drawer=!drawer")
 </template>
 
 <script>
 export default {
   name: 'HeaderMenuOnMobile',
   data: () => ({
-      drawer: null,
-  })
+      drawer: false,
+      linkItems: [
+        {
+          path: '/',
+          name: 'home',
+          icon: 'home'
+        },
+        {
+          path: '/history',
+          name: 'history',
+          icon: 'home'
+        },
+        {
+          path: '/works',
+          name: 'works',
+          icon: 'home'
+        },
+        {
+          path: '/analytics',
+          name: 'analytics',
+          icon: 'home'
+        },
+        {
+          path: '/future',
+          name: 'future',
+          icon: 'home'
+        }
+      ]
+  }),
+  methods: {
+    toLink (link) {
+      this.$router.push(link)
+    }
+  }
 };
 </script>
 
 <style scoped>
-.link {
-  text-decoration: none;
-}
-/* .app-bar {
-  color: var(--light);
-} */
+
 </style>
